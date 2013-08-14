47841d01c48168875235eab112bd2d79e181f496
Lucas Chaufournier <lucasch@gwmail.gwu.edu>
2013-08-14 14:51:42 -0400

    Added beginning of a script to commission new machine install

---
+++ comissionComputer.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+cd
+mkdir setup
+cd setup
+echo "###################Installing software####################";
+sudo apt-get install -y  git terminator clusterssh htop atop iftop vim 
+3
+echo "###################Installing chrome#####################";
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i ./google-chrome*.deb
+sudo apt-get install -f
+
+echo "#################Installing Dropbox#######################";
+wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf .
+
+~/.dropbox-dist/dropboxd
+
+echo "################Updating System##########################";
+sudo apt-get update
+sudo apt-get upgrade
+
+echo "#################Removing setup file";
+cd
+sudo rm -r setup
+echo "##############Machine setup complete#####################";
+
